7ed77609e818c95fb5e8d032c8fb892fbf78333a
vaperLee <rize1126jesse@gmail.com>
2022-06-10 23:22:31 -0400

    create tickers and can see other comany stock info

--- app.py
+++ app.py
@@ -4,13 +4,23 @@ import yfinance as yf
 
 # %matplotlib inline
 
-aapl = yf.Ticker('AAPL')
+#aapl = yf.Ticker('AAPL')
 
 days = 20                                           #range
-hist = aapl.history(period=f'{days}d')
+
+tickers = {
+    'apple': 'AAPL',
+    'facebook': 'FB'
+
+}
+
+company = 'facebook'
+
+tkr = yf.Ticker(tickers[company])
+hist = tkr.history(period=f'{days}d')              #fetch stock infomation
 hist.index = hist.index.strftime('%d %B %Y')        #change date format
 hist = hist[['Close']]                              #Get only the latest stock price
-hist.columns = ['apple']                            #put name of company on the column
+hist.columns = [company]                            #put name of company on the column
 hist = hist.T                                       #Turn the table sideways
 hist.index.name = 'Name'                            #change the name
 hist
