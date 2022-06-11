17fd42b8684d3dc288135db8875d274f7ba2460b
vaperLee <rize1126jesse@gmail.com>
2022-06-10 23:45:54 -0400

    Implemented to see stock prices of all companies

--- app.py
+++ app.py
@@ -2,25 +2,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-# %matplotlib inline
-
-#aapl = yf.Ticker('AAPL')
 
 days = 20                                           #range
 
 tickers = {
-    'apple': 'AAPL',
-    'facebook': 'FB'
+    'Apple': 'AAPL',
+    'Facebook': 'FB',
+    'Google': 'GOOGL',
+    'Microsoft': 'MSFT',
+    'Netflix': 'NFLX',
+    'Amazon': 'AMZN'
 
 }
 
-company = 'facebook'
+def get_data(days, tickers):
+    df = pd.DataFrame()  # Container for info
 
-tkr = yf.Ticker(tickers[company])
-hist = tkr.history(period=f'{days}d')              #fetch stock infomation
-hist.index = hist.index.strftime('%d %B %Y')        #change date format
-hist = hist[['Close']]                              #Get only the latest stock price
-hist.columns = [company]                            #put name of company on the column
-hist = hist.T                                       #Turn the table sideways
-hist.index.name = 'Name'                            #change the name
-hist
+    for company in tickers.keys():
+        tkr = yf.Ticker(tickers[company])
+        hist = tkr.history(period=f'{days}d')  # fetch stock infomation
+        hist.index = hist.index.strftime('%d %B %Y')  # change date format
+        hist = hist[['Close']]  # Get only the latest stock price
+        hist.columns = [company]  # put name of company on the column
+        hist = hist.T  # Turn the table sideways
+        hist.index.name = 'Name'  # change the name
+        df = pd.concat([df, hist])  # update
+    return df
+
+get_data(days, tickers)
+
+
+
